bfc6bdc5dfbb11beabfa06bd713d338f11d64cb1
Constantin Pape <constantin.pape@informatik.uni-goettingen.de>
2026-06-21 14:55:55 +0200

    Add PyPI publication workflow and bump version (#7)

--- bioimage_py/__init__.py
+++ bioimage_py/__init__.py
@@ -9,9 +9,10 @@ from .downsample import downsample
 from .runner import get_runner
 from .sources import as_source, open_cloudvolume, open_source, open_webknossos
 from .util import to_roi
+from .__version__ import __version__
 
-__version__ = "0.0.1"
 __all__ = [
+    "__version__",
     "stats",
     "filters",
     "segmentation",
